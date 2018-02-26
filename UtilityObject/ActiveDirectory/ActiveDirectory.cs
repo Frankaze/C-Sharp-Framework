d254031757db3a6314ebcf4c25db27f33e129af6
FRANKAZE-MBP\Frankaze <frankaze@gmail.com>
2018-02-26 23:40:38 +0800

    加入專案檔案。

---
+++ UtilityObject/ActiveDirectory/ActiveDirectory.cs
@@ -0,0 +1,39 @@
+﻿using System.DirectoryServices;
+
+namespace UtilityObject.ActiveDirectory
+{
+    public static class ActiveDirectory
+    {
+        /// <summary>
+        /// 連接Active Directory驗證帳號密碼
+        /// </summary>
+        /// <param name="strDomain">Active Directory的網域名稱或IP</param>
+        /// <param name="strUserName">登入帳號</param>
+        /// <param name="strPassword">登入密碼</param>
+        /// <returns></returns>
+        public static bool IsAuthenticated(string strDomain, string strUserName, string strPassword)
+        {
+            DirectoryEntry _objDirectoryEntry = new DirectoryEntry("LDAP://" + strDomain, strUserName, strPassword);
+
+            DirectorySearcher _objDirectorySearcher = new DirectorySearcher(_objDirectoryEntry);
+            _objDirectorySearcher.Filter = "(sAMAccountName=" + strUserName + ")";
+            try
+            {
+                SearchResult _objSearchResult = _objDirectorySearcher.FindOne();
+
+                if (_objSearchResult != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ UtilityObject/ParseEnum/ParseEnum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UtilityObject.ParseEnum
+{
+    public static class ParseEnum
+    {
+        /// <summary>
+        /// 將字串轉換成Enum
+        /// </summary>
+        /// <typeparam name="TEnum">Enum類別</typeparam>
+        /// <param name="str">字串</param>
+        /// <returns>Enum的值</returns>
+        public static TEnum ParseStringToEnum<TEnum>(string str)
+        {
+            return (TEnum)Enum.Parse(typeof(TEnum), str, true);
+        }
+
+        /// <summary>
+        /// 將字串轉換成Enum，如果傳入空白則回傳Null
+        /// </summary>
+        /// <typeparam name="TEnum">Enum類別</typeparam>
+        /// <param name="str">字串</param>
+        /// <returns>Enum的值 或 Null</returns>
+        public static TEnum? ParseStringToEnumIncludeNull<TEnum>(string str) where TEnum : struct
+        {
+            if (string.IsNullOrWhiteSpace(str) == true)
+            {
+                return (TEnum?)null;
+            }
+            else
+            {
+                return (TEnum)Enum.Parse(typeof(TEnum), str, true);
+            }
+        }
+    }
+}
