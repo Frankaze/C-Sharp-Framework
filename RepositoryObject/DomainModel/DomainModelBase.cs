2df97b44d9b96b3d95aeca6f56cbd79b5ce943b8
FRANKAZE-MBP\Frankaze <frankaze@gmail.com>
2019-12-18 02:33:49 +0800

    Add Domain Model

---
+++ RepositoryObject/DomainModel/DomainModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryObject.DomainModel
+{
+    public class DomainModelBase
+    {
+        #region Constant
+
+        #endregion Constant
+
+        #region Variable
+
+        #endregion Variable
+
+        #region Property
+
+        #endregion Property
+
+        #region Constructor
+
+        #endregion Constructor
+
+        #region Function
+
+        #endregion Function
+    }
+}
