84ee367d77edb8cc13fc289f8d0ee84c6c5ebbe9
Frankaze <frankaze@gmail.com>
2020-05-29 23:17:32 +0800

    Add List Extension

---
+++ UtilityObject/Extension/ListExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UtilityObject.Extension
+{
+    public static class ListExtension
+    {
+        #region Constant
+
+        #endregion Constant
+
+        #region Variable
+
+        #endregion Variable
+
+        #region Property
+
+        #endregion Property
+
+        #region Constructor
+
+        #endregion Constructor
+
+        #region Function
+
+        public static IList<T> Clone<T>(this IList<T> list) where T : ICloneable
+        {
+            return list.Select(i => (T)i.Clone()).ToList();
+        }
+
+        #endregion Function
+
+        #region Class
+
+        #endregion Class
+    }
+}
