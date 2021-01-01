aa63191af4906aad499259e29017650e14268895
Frankaze <frankaze@gmail.com>
2021-01-01 23:32:12 +0800

    Add Encrpytion

---
+++ UtilityObject/Encrpytion/Encrpytion.cs
@@ -0,0 +1,72 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace UtilityObject.Encrpytion
+{
+    public static class Encrpytion
+    {
+        #region Constant
+
+        #endregion Constant
+
+        #region Variable
+
+        #endregion Variable
+
+        #region Property
+
+        #endregion Property
+
+        #region Constructor
+
+        #endregion Constructor
+
+        #region Function
+
+        /// <summary>
+        /// 使用MD5加密
+        /// </summary>
+        /// <param name="enumCase">大小寫</param>
+        /// <param name="strPlaintext">明文</param>
+        /// <returns>密文</returns>
+        public static string EncryptByMD5(CaseEnum enumCase, string strPlaintext)
+        {
+            MD5 _md5 = new MD5CryptoServiceProvider();
+
+            byte[] _byteBufferArray = Encoding.Default.GetBytes(strPlaintext);
+            byte[] _byteMD5Array = new MD5CryptoServiceProvider().ComputeHash(_byteBufferArray);
+
+            StringBuilder _sb = new StringBuilder();
+            for (int i = 0; i < _byteMD5Array.Length; i++)
+            {
+                switch (enumCase)
+                {
+                    case CaseEnum.Lowercase:
+                        _sb.Append(_byteMD5Array[i].ToString("x2"));
+                        break;
+
+                    case CaseEnum.Uppercase:
+                        _sb.Append(_byteMD5Array[i].ToString("X2"));
+                        break;
+                }
+            }
+            return _sb.ToString();
+        }
+
+        #endregion Function
+
+        #region Class
+
+        #endregion Class
+
+        #region Enum
+
+        public enum CaseEnum
+        {
+            Uppercase,
+            Lowercase,
+        }
+
+        #endregion Enum
+    }
+}
