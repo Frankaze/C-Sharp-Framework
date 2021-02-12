797b3720342ed77c147a3bac95ba7ec5eb34d3a3
Frankaze <frankaze@gmail.com>
2021-02-12 23:42:43 +0800

    Add EnumExtension & Modify DateTimeExtension

--- UtilityObject/Extension/DateTimeExtension.cs
+++ UtilityObject/Extension/DateTimeExtension.cs
@@ -13,5 +13,34 @@ namespace UtilityObject.Extension
         {
             return date.ToUniversalTime().AddHours(8);
         }
+
+        /// <summary>
+        /// 取得當週第一天 (星期一開始)
+        /// </summary>
+        /// <param name="date">時間</param>
+        /// <returns>當週第一天</returns>
+        public static DateTime FirstDayOfWeekFromMonday(this DateTime date)
+        {
+            int _intDayOfWeek = Convert.ToInt32(date.DayOfWeek);
+
+            //如果遇到星期天 (7-1)
+            int _intDayDiff = (-1) * (_intDayOfWeek == 0 ? (7 - 1) : (_intDayOfWeek - 1));
+
+            return date.AddDays(_intDayDiff).Date;
+        }
+
+        /// <summary>
+        ///  取得當週最後一天 (星期日結束)
+        /// </summary>
+        /// <param name="date">時間</param>
+        /// <returns>當週最後一天</returns>
+        public static DateTime LastDayOfWeekToSunday(this DateTime date)
+        {
+            int _intDayOfWeek = Convert.ToInt32(date.DayOfWeek);
+
+            int _intDayDiff = 7 - (_intDayOfWeek == 0 ? 7 : _intDayOfWeek);
+
+            return date.AddDays(_intDayDiff).Date;
+        }
     }
 }
---
+++ UtilityObject/Extension/EnumExtension.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace UtilityObject.Extension
+{
+    public static class EnumExtension
+    {
+        #region Constant
+
+        #endregion Constant
+
+        #region Variable
+
+        #endregion Variable
+
+        #region Property
+
+        #endregion Property
+
+        #region Constructor
+
+        #endregion Constructor
+
+        #region Function
+
+        /// <summary>
+        /// 取得ComponentModel的描述
+        /// </summary>
+        public static string GetDescription<T>(this T source)
+        {
+            FieldInfo _fi = source.GetType().GetField(source.ToString());
+
+            DescriptionAttribute[] _attribute = (DescriptionAttribute[])_fi.GetCustomAttributes(
+             typeof(DescriptionAttribute), false);
+
+            if (_attribute.Length > 0)
+            {
+                return _attribute[0].Description;
+            }
+            else
+            {
+                return source.ToString();
+            }
+        }
+
+        #endregion Function
+
+        #region Class
+
+        #endregion Class
+    }
+}
